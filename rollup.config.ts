eb0bcbd23b24ba4d24a05d778ff2c9d686905bbb
Danil Ovchinnikov <danil42worker@gmail.com>
2026-02-17 02:26:55 +0300

    chore: build configuration (#5)

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,12 @@
-// See: https://rollupjs.org/introduction/
-
 import commonjs from "@rollup/plugin-commonjs"
 import nodeResolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript"
 
 const config = {
-  input: "src/index.ts",
+  input: "src/setup-acton.ts",
   output: {
-    esModule: true,
     file: "dist/index.js",
     format: "es",
-    sourcemap: true,
   },
   plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
 }
