9579ba987a136f332ccd31820d6e6262f019bdd4
Danil Ovchinnikov <danil42worker@gmail.com>
2026-04-29 03:37:19 +0300

    chore: update coverage exclude pattern

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     clearMocks: true,
     coverage: {
       all: true,
-      exclude: ["dist/**", "node_modules/**"],
+      exclude: ["dist/**", "node_modules/**", "src/download-current-checksums.ts"],
       include: ["src/**"],
       provider: "v8",
       reportsDirectory: "./coverage",
